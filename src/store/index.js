c29812746bc2cb5a29e3c4e6a6dcffba5dde4468
VianneyR4 <vianneyrwicha2017@gmail.com>
2021-09-27 10:54:44 +0200

    Feat:: Get Images  API integration

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    image_path : "http://172.18.2.105:6300/",
+    image_path : "http://localhost:3000/",
     api_key:
       "RoddXUVwoAtK0IbM6ugGNENizaAEam5bWUgBdfvurZ0e3eiMRe9DMRPdh55nrmNW7Y5oLdOchGFRdhuUuOS39ySsSONr3Xbr9viYpSGhxvgAcXUgJlVuPlFhdQEYNhVTpC8jgjkghjhka787eJHJKHujyuhsa6",
     api_app_chanel: "Admin",
